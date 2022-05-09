7ebde8ca40078bfb614830277dacbe7784935b8b
Hoerin Doh <hellodhlyn@gmail.com>
2022-05-09 22:43:03 +0900

    Add notification for talk channel posts

--- umapyoi-bot/src/bindings.d.ts
+++ umapyoi-bot/src/bindings.d.ts
@@ -4,10 +4,34 @@ declare global {
   const UMAPYOI_BOT_KV: KVNamespace;
 
   const DISCORD_WEBHOOK_URL: string;
+  const PF_CHANNEL_URL: string;
   const TWITTER_BEARER_TOKEN: string;
 
   type Tweet = {
     text: string;
     id: string;
-  }
+  };
+
+  type PFChannel = {
+    posts: {
+      items: PFPostItem[];
+    };
+  };
+
+  type PFPostItem = {
+    title: string;
+    created_at: number;
+    media: PFPostItemMedia[];
+    contents: PFPostItemContent[];
+    permalink: string;
+  };
+
+  type PFPostItemContent = {
+    t: string;
+    v: string;
+  };
+
+  type PFPostItemMedia = {
+    xlarge_url: string;
+  };
 }
--- umapyoi-bot/src/index.ts
+++ umapyoi-bot/src/index.ts
@@ -21,6 +21,12 @@ async function handleSchedule(time) {
 }
 
 async function handle(): Promise<Response> {
+  await fetchTweets();
+  await fetchChannel();
+  return new Response("ok");
+}
+
+async function fetchTweets() {
   const lastTweetIdKey = "LAST_TWEET_ID";
   const lastTweetId = await UMAPYOI_BOT_KV.get(lastTweetIdKey);
   const tweetUrl = `https://api.twitter.com/2/users/1512011570955841539/tweets?since_id=${lastTweetId}`;
@@ -32,20 +38,48 @@ async function handle(): Promise<Response> {
 
   const resBody = await response.json<{ data: Tweet[] }>();
   if (!resBody.data || resBody.data.length === 0) {
-    return new Response("ok");
+    return;
   }
 
   const tweets = resBody.data.sort((a, b) => a.id - b.id);
+  await sendWebhook(tweets.map((t) => t.text).join("\n\n"));
+
+  await UMAPYOI_BOT_KV.put(lastTweetIdKey, tweets[tweets.length - 1].id);
+}
+
+async function fetchChannel() {
+  const lastCreatedAtKey = "LAST_POST_CREATED_AT";
+  const lastCreatedAt = parseInt(await UMAPYOI_BOT_KV.get(lastCreatedAtKey));
+
+  const response = await fetch(PF_CHANNEL_URL);
+  const channel = await response.json<PFChannel>();
+  const posts = channel.posts.items
+    .filter((i) => i.created_at > lastCreatedAt)
+    .sort((a, b) => a.created_at - b.created_at);
+  if (posts.length === 0) {
+    return;
+  }
+
+  const contents = posts.map((p) => {
+    const body = p.contents.map((c) => c.v).join("\n\n");
+    let postContent = `**${p.title}**\n\n${body}`;
+    if (p.media && p.media.length > 0) {
+      postContent += `\n${p.media[0].xlarge_url}`;
+    }
+    postContent += `\n\n더 보기 : ${p.permalink}`;
+    return postContent;
+  }).join("\n---\n");
+  await sendWebhook(contents);
+
+  await UMAPYOI_BOT_KV.put(lastCreatedAtKey, posts[posts.length - 1].created_at.toString());
+}
+
+async function sendWebhook(content: string) {
   await fetch(DISCORD_WEBHOOK_URL, {
     method: "POST",
     headers: {
       "Content-Type": "application/json; charset=utf-8",
     },
-    body: JSON.stringify({
-      content: tweets.map((t) => t.text).join("\n\n"),
-    }),
+    body: JSON.stringify({ content }),
   });
-
-  await UMAPYOI_BOT_KV.put(lastTweetIdKey, tweets[tweets.length - 1].id);
-  return new Response("ok");
 }
